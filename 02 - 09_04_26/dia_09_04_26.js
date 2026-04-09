d9ba0374aa1cd02d64cfbe60295a043a9c05f63e
thahrockpop8-dotcom <thahrockpop8@gmail.com>
2026-04-09 11:33:15 -0300

    Update dia_09_04_26.js
    javascript

--- 02 - 09_04_26/dia_09_04_26.js
+++ 02 - 09_04_26/dia_09_04_26.js
@@ -2,17 +2,40 @@ function pedirNome() {
     let nome;
     let regexNome = /^[A-Za-zÀ-ÿ\s'-]+$/;
     while (true) {
-        nome  = prompt("Digite o seu nome completo:") ;
+        nome  = prompt("Digite o seu nome completo:"); //condição
            if (nome === null){
            alert("O nome não pode ficar vazio. ")
            continue;
 
         }
         if(!regexNome.test(nome)){
-         alert("Digite apenas letras e espaços no nome.");
+         alert("Digite apenas letras e espaços no nome.")
          continue;   
         }
-        
+
+        let partesNome = nome.split(/\s+/);
+        if (partesNome.lengt < 2){
+            alert ("Digite o nome completo, com nome e sobrenome.") //condição
+            continue;
+        }
+        if (nome.length < 3){
+            alert("Digite um nome válido.") //condição
+            continue;
+        }
+        return nome; //se passou por todas as validações, retorna o nome
     }
+
+}
+
+function iniciarCadastro(){
+    let nome = pedirNome ();
+    if (nome === null){
+        document.getElementById("resultado").innerHTML = 
+        "<h2>Cadastro cancelado</h2><p>O usuário cancelou o preenchimento</p>"
+        return;
+    }
+    document.getElementById("resultado").innerHTML = 
+    "<h2>Resultado</h2>" + 
+    "<p><strong>Nome Completo: <strong>" + nome + "</p>"
 
 }
