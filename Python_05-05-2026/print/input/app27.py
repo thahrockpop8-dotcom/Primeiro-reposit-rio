cb4501e870b06c9a9c775fe77f7eefafd55d4148
thahrockpop8-dotcom <thahrockpop8@gmail.com>
2026-05-19 11:58:35 -0300

    Novo exercicio
    Exercício 6

--- Python_05-05-2026/print/input/app27.py
+++ Python_05-05-2026/print/input/app27.py
@@ -27,7 +27,7 @@ elif nota >= 5:
 
 #-------------------------------------------------------------->
 
-#Exemplo
+#Exemplo 1 
 
 idade = int(input("Quantos anos você tem? "))
 if idade >= 18:
@@ -36,6 +36,26 @@ if idade >= 18:
 else: 
     print ("Você não pode tirar carteira de motorista.")
 
-    
+#--------------------------------------------------------------->
 
+#Exemplo 2 
+resposta = input ("Python é uma linguagem de programação? ")
+if resposta == "sim":
+    print ("Correto!")
+else:
+    print ("Incorreto.")
 
+#------------------------------------------------------------------>
+
+#Exemplo 3
+pets = input("Você possui algum animal de estimação? ").lower()
+
+if pets == "sim":
+    print("Que legal! Adoraria saber mais sobre seu animalzinho.")
+    nome_pet = input("Qual o nome do seu animalzinho? ")
+    print(f"O nome dele(a) é {nome_pet}.")
+
+elif pets == "não":
+    print("Isso é uma pena. Você sabia que existem estudos que comprovam que animais de estimação ajudam na saúde mental? \nSe você sempre quis ter um e sempre hesitou, não perca tempo! \nEles são os melhores companheiros que alguém poderia ter.")
+
+#---------------------------------------------------------------------->
